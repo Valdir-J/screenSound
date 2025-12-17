8f0836d517360ec7df07bd76dad83799b270ecd1
Valdir-J <algumascoisasaleatorias@gmail.com>
2025-12-17 15:20:02 -0300

    Initial Commit

---
+++ src/main/java/com/example/screenSound/ScreenSoundApplication.java
@@ -0,0 +1,23 @@
+package com.example.screenSound;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSoundApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(ScreenSoundApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
