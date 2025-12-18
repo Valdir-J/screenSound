e1032a63341dd10b0e2c6dcb1892b43cf49ad693
Valdir-J <algumascoisasaleatorias@gmail.com>
2025-12-18 15:01:28 -0300

    feat: cria interfaces de repositório para Artista e Musica

---
+++ src/main/java/com/example/screenSound/repository/ArtistaRepository.java
@@ -0,0 +1,7 @@
+package com.example.screenSound.repository;
+
+import com.example.screenSound.model.Artista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtistaRepository extends JpaRepository<Artista, Long> {
+}
---
+++ src/main/java/com/example/screenSound/repository/MusicaRepository.java
@@ -0,0 +1,7 @@
+package com.example.screenSound.repository;
+
+import com.example.screenSound.model.Musica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MusicaRepository extends JpaRepository<Musica, Long> {
+}
