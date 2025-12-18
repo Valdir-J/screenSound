6fe1c5f603d61bea14568ea465d75fee16427dc4
Valdir-J <algumascoisasaleatorias@gmail.com>
2025-12-18 15:11:50 -0300

    feat: faz injeção de dependências dos repositórios via construtor na classe Principal

--- src/main/java/com/example/screenSound/ScreenSoundApplication.java
+++ src/main/java/com/example/screenSound/ScreenSoundApplication.java
@@ -1,13 +1,21 @@
 package com.example.screenSound;
 
 import com.example.screenSound.principal.Principal;
+import com.example.screenSound.repository.ArtistaRepository;
+import com.example.screenSound.repository.MusicaRepository;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenSoundApplication implements CommandLineRunner {
+
+    @Autowired
+    private ArtistaRepository artistaRepository;
+    @Autowired
+    private MusicaRepository musicaRepository;
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
@@ -19,7 +27,7 @@ public class ScreenSoundApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Principal principal = new Principal();
+        Principal principal = new Principal(artistaRepository, musicaRepository);
         principal.exibeMenu();
     }
 }
--- src/main/java/com/example/screenSound/principal/Principal.java
+++ src/main/java/com/example/screenSound/principal/Principal.java
@@ -1,8 +1,20 @@
 package com.example.screenSound.principal;
 
-import java.util.Scanner;
+import com.example.screenSound.repository.ArtistaRepository;
+import com.example.screenSound.repository.MusicaRepository;
 
 public class Principal {
+    private ArtistaRepository artistaRepository;
+    private MusicaRepository musicaRepository;
+
+    public Principal() {
+    }
+
+    public Principal(ArtistaRepository artistaRepository,
+                     MusicaRepository musicaRepository) {
+        this.artistaRepository = artistaRepository;
+        this.musicaRepository = musicaRepository;
+    }
 
     public void exibeMenu() {
         System.out.println("Hello World!");
