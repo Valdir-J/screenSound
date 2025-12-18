f741ff438759811642098b53063e15e9e2f62ba1
Valdir-J <algumascoisasaleatorias@gmail.com>
2025-12-18 15:17:27 -0300

    feat: implementa o cadastro dos artistas no banco de dados

--- src/main/java/com/example/screenSound/principal/Principal.java
+++ src/main/java/com/example/screenSound/principal/Principal.java
@@ -1,13 +1,21 @@
 package com.example.screenSound.principal;
 
+import com.example.screenSound.model.Artista;
+import com.example.screenSound.model.TipoArtista;
 import com.example.screenSound.repository.ArtistaRepository;
 import com.example.screenSound.repository.MusicaRepository;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Principal {
+    private final Scanner scanner = new Scanner(System.in);
     private ArtistaRepository artistaRepository;
     private MusicaRepository musicaRepository;
 
     public Principal() {
+
     }
 
     public Principal(ArtistaRepository artistaRepository,
@@ -17,6 +25,58 @@ public class Principal {
     }
 
     public void exibeMenu() {
-        System.out.println("Hello World!");
+        int opcao = -1;
+        String menu = """
+                
+                ---- Screen Sound Músics ----
+                
+                1 - Cadastrar Artistas
+                2 - Cadastrar Músicas
+                3 - Listar Músicas 
+                4 - Buscar músicas por artista
+                5 - Pesquisar dados sobre um artista
+                
+                0 - Sair
+                """;
+
+        while (opcao != 0) {
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    criarArtistas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+
+        }
+    }
+
+    private void criarArtistas() {
+        Set<Artista> artistas = new HashSet<>();
+        String opcao = "S";
+
+        while (opcao.equalsIgnoreCase("S")) {
+            System.out.println("Qual é o nome do artista?");
+            var nomeArtista = scanner.nextLine();
+
+            System.out.println("Qual é o tipo desse artista? (solo, dupla, banda)");
+            var tipo = scanner.nextLine();
+            TipoArtista tipoArtista = TipoArtista.fromString(tipo);
+
+            Artista artista = new Artista(nomeArtista, tipoArtista);
+            artistas.add(artista);
+
+            System.out.println("Cadastrar outro artista? (S/N)");
+            opcao = scanner.nextLine().trim();
+        }
+        artistaRepository.saveAll(artistas);
+        System.out.println("Artistas cadastrados com sucesso!");
     }
 }
